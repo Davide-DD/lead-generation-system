0c12b26a01b75f6d0bc6c7ef4d1d6e6021dde222
Davide Di Donato <davidedidonato94@gmail.com>
2018-10-21 18:35:30 +0200

    Added form validation

--- logic.js
+++ logic.js
@@ -1,10 +1,40 @@
 var serviceSelectedId = null
 var hasFirstSelected = false
 
+$(document).ready(function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    $('.needs-validation').submit(function (event) {
+        if ($(this)[0].checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        else {
+            // Trovo il servizio selezionato
+            var serviceSelected = $('#' + serviceSelectedId)
+            console.log(serviceSelected)
+
+            // Trovo i servizi aggiuntivi spuntati
+            var checkboxesSelected = $('input[name="checkboxes"]:checked')
+            console.log(checkboxesSelected)
+
+            // Trovo per quanto tempo sono richiesti i servizi
+            var radioSelected = $('input[name="radios"]:checked')[0]
+            console.log(radioSelected)
+
+            // Trovo i campi di nome, cognome e e-mail
+            var name = $("#name").val()
+            var surname = $("#surname").val()
+            var email = $("#email").val()
+            console.log(name + "," + surname + "," + email)
+        }
+        $(this)[0].classList.add('was-validated');
+    })
+});
+
 function findBg(element) {
     var classList = element.attr('class').split(/\s+/);
     var bg = null
-    $.each(classList, function(index, item) {
+    $.each(classList, function (index, item) {
         if (index === 2) {
             bg = item
         }
@@ -16,45 +46,13 @@ $(".service").on("click", function (event) {
     serviceSelectedId = $(this).attr('id')
     $(".service").each(function (i) {
         if (serviceSelectedId !== $(this).attr('id')) {
-            if (!hasFirstSelected) {
-                var classToRemove = findBg($(this))
-                $(this).removeClass(findBg($(this))).addClass("bg-secondary")
-            } 
-            else {
-                $(this).removeClass("bg-success").addClass("bg-secondary")
-            }
+            if (!hasFirstSelected) $(this).removeClass(findBg($(this))).addClass("bg-secondary")
+            else $(this).removeClass("bg-success").addClass("bg-secondary")
         }
         else {
-            if (!hasFirstSelected) {
-                var classToRemove = findBg($(this))
-                $(this).removeClass(classToRemove).addClass("bg-success")
-            } 
-            else {
-                $(this).removeClass("bg-secondary").addClass("bg-success")
-            }
+            if (!hasFirstSelected) $(this).removeClass(findBg($(this))).addClass("bg-success")
+            else $(this).removeClass("bg-secondary").addClass("bg-success")
         }
     });
     hasFirstSelected = true
-});
-
-$( "#confirm" ).click(function(event) {
-    event.preventDefault()
-    
-    // Trovo il servizio selezionato
-    var serviceSelected = $('#' + serviceSelectedId)
-    console.log(serviceSelected)
-
-    // Trovo i servizi aggiuntivi spuntati
-    var checkboxesSelected = $('input[name="checkboxes"]:checked')
-    console.log(checkboxesSelected)
-
-    // Trovo per quanto tempo sono richiesti i servizi
-    var radioSelected = $('input[name="radios"]:checked')[0]
-    console.log(radioSelected)
-
-    // Trovo i campi di nome, cognome e e-mail
-    var name = $("#name").val()
-    var surname = $("#surname").val()
-    var email = $("#email").val()
-    console.log(name + "," + surname + "," + email)
 });
